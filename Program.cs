12c0ba175325902cf55911705c146a448eb1008e
kundan <goswamikundan123@gmail.com>
2023-02-08 07:41:06 +0530

    first file commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Helloworld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //int num = 7;
+            //double num1 = 10.9;
+            //bool val = true;
+            //char single = 's';
+            //string name = "bridgelab";
+
+            //Console.WriteLine("Hi, Hello how are you?");
+
+            //Console.WriteLine("This is an Integer value:" +num +num1 +val +single);
+            //Console.WriteLine("this is a double value:" + num1);
+            //Console.WriteLine("this is a boolean value:" + val);
+            //Console.WriteLine("this is a character value:" + single);
+            //Console.WriteLine("this is an string value:" + name);
+
+            //string nameofprsn=Console.ReadLine();
+            //Console.WriteLine("The name is:"+nameofprsn);
+            //Console.WriteLine("The name is {0} {1} {2} {3}", nameofprsn,num,num1,val);
+
+
+
+            //Console.WriteLine("Enter the first number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the second number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num3 = num1 + num2;
+            //Console.WriteLine("The addition of two numbers " + num3);
+
+
+            //Console.WriteLine(username.a);
+            //username.Displayusername();
+
+            Flipcoin.calculatepercentage();
+        }
+    }
+}
