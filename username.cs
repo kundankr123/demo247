a39b967263301383241d61aed1c9f25648e7d172
kundan <goswamikundan123@gmail.com>
2023-02-08 07:43:40 +0530

    input username file

---
+++ username.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class username
+    {
+        public static int a = 10;
+        public static void Displayusername()
+        {
+            Console.WriteLine("Print the username");
+            string username = Console.ReadLine();
+            int len = username.Length;
+            Console.WriteLine("The number of characters is:" + len);
+            if (username.Length >= 3 && username.Length <= 5)
+            {
+                Console.WriteLine("Hello " + username + ", How are you?");
+                Console.WriteLine("Hello {0}, How are you?", username);
+            }
+            else if (username.Length > 5)
+            {
+                Console.WriteLine("the name is having more than 5 characters.");
+            }
+            else
+            {
+                Console.WriteLine("The name is not having mimnimum 3 characters.");
+            }
+        }
+    }
+}
